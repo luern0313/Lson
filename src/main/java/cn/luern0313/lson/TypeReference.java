427fe60ec97362e219b5df7122b9638614bb558a
luern0313 <liupeiran0313@163.com>
2021-12-23 00:15:25 +0800

    优化typeReference类

--- src/main/java/cn/luern0313/lson/TypeReference.java
+++ src/main/java/cn/luern0313/lson/TypeReference.java
@@ -16,41 +16,38 @@ public class TypeReference<T>
     public Type type;
     public Class<?> rawType;
 
-    public LinkedHashMap<String, TypeReference<?>> typeMap = new LinkedHashMap<>();
+    public LinkedHashMap<String, TypeReference<?>> typeMap;
 
     public TypeReference()
     {
         Type superClass = getClass().getGenericSuperclass();
-        type = ((ParameterizedType) superClass).getActualTypeArguments()[0];
-        rawType = (Class<?>) ((ParameterizedType) type).getRawType();
 
-        Type[] parameterizedTypes = ((ParameterizedType) type).getActualTypeArguments();
-        TypeVariable<? extends Class<?>>[] typeVariables = rawType.getTypeParameters();
-        for (int i = 0; i < parameterizedTypes.length; i++)
-            typeMap.put(typeVariables[i].getName(), handleType(parameterizedTypes[i]));
-    }
-
-    public TypeReference(Class<?> clz)
-    {
-        this.type = clz;
-        this.rawType = clz;
-    }
-
-    private TypeReference<?> handleType(Type parameterizedType)
-    {
-        if(parameterizedType instanceof ParameterizedType)
+        if(superClass instanceof ParameterizedType)
         {
-            Class<?> nextClass = (Class<?>) ((ParameterizedType) parameterizedType).getRawType();
-            TypeReference<?> typeReference = new TypeReference<>(nextClass);
-
-            Type[] parameterizedTypes = ((ParameterizedType) parameterizedType).getActualTypeArguments();
-            TypeVariable<? extends Class<?>>[] typeParameterizedArray = nextClass.getTypeParameters();
-            for (int j = 0; j < parameterizedTypes.length; j++)
-                typeReference.typeMap.put(typeParameterizedArray[j].getName(), handleType(parameterizedTypes[j]));
-            return typeReference;
+            type = ((ParameterizedType) superClass).getActualTypeArguments()[0];
+            handleType();
         }
-        else if(parameterizedType instanceof Class)
-            return new TypeReference<>((Class<?>) parameterizedType);
-        return new TypeReference<>(null);
+    }
+
+    public TypeReference(Type type)
+    {
+        this.type = type;
+        handleType();
+    }
+
+    private void handleType()
+    {
+        if(type instanceof ParameterizedType)
+        {
+            rawType = (Class<?>) ((ParameterizedType) type).getRawType();
+
+            typeMap = new LinkedHashMap<>();
+            Type[] parameterizedTypes = ((ParameterizedType) type).getActualTypeArguments();
+            TypeVariable<? extends Class<?>>[] typeVariables = rawType.getTypeParameters();
+            for (int i = 0; i < parameterizedTypes.length; i++)
+                typeMap.put(typeVariables[i].getName(), new TypeReference<>(parameterizedTypes[i]));
+        }
+        else if(type instanceof Class<?>)
+            rawType = (Class<?>) type;
     }
 }
