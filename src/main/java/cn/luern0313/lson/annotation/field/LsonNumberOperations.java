fa4829f78fb0fd94227926c02fff3b7532576273
luern0313 <liupeiran0313@163.com>
2022-01-05 00:09:07 +0800

    数字运算注解增加转int方法

--- src/main/java/cn/luern0313/lson/annotation/field/LsonNumberOperations.java
+++ src/main/java/cn/luern0313/lson/annotation/field/LsonNumberOperations.java
@@ -13,6 +13,9 @@ import cn.luern0313.lson.annotation.other.AnnotationOrder;
  *
  * <p>注明运算符和数字时，请以反序列化为标准注明。序列化时会自动反向运算。
  *
+ * <p>{@link LsonNumberOperations#isCastInteger()} 为true时可以在反序列化时将结果向下求整，
+ * 但序列化时将不会恢复，请注意。
+ *
  * <p>反序列化中：输入{@code Number}类型，输出{@code Number}类型。
  * <p>序列化中：输入{@code Number}类型，输出{@code Number}类型。
  *
@@ -41,11 +44,21 @@ public @interface LsonNumberOperations
     double number();
 
     /**
+     * 是否将最终数字转为int，可去除小数点和小数点后的数字。
+     *
+     * <p>通常在计算数字后结果为整数时使用，若不为整数则将向下取整。
+     *
+     * @return 是否转为Integer
+     */
+    boolean isCastInteger() default false;
+
+    /**
      * 用于排序注解的执行顺序，见{@link AnnotationOrder}。
      * @return 注解执行顺序
      */
     @AnnotationOrder int order() default Integer.MAX_VALUE;
 
+    //TODO 支持求余等更多运算符
     enum Operator
     {
         /**
@@ -74,17 +87,21 @@ public @interface LsonNumberOperations
         @Override
         public Object deserialization(Object value, LsonNumberOperations lsonNumberOperations, Object object)
         {
-            return operationsHandler((Double) value, lsonNumberOperations.operator(), lsonNumberOperations.number());
+            double result = operationsHandler((Double) value, lsonNumberOperations.operator(), lsonNumberOperations.number());
+            if(lsonNumberOperations.isCastInteger())
+                return (int) result;
+            else
+                return result;
         }
 
         @Override
         public Object serialization(Object value, LsonNumberOperations lsonNumberOperations, Object object)
         {
             Operator operator = Operator.values()[lsonNumberOperations.operator().ordinal() - (lsonNumberOperations.operator().ordinal() % 2 * 2 - 1)];
-            return operationsHandler(lsonNumberOperations.number(), operator, (Double) value);
+            return operationsHandler((Double) value, operator, lsonNumberOperations.number());
         }
 
-        private double operationsHandler(double left, Operator operator, double right)
+        private Double operationsHandler(double left, Operator operator, double right)
         {
             if(operator == Operator.ADD)
                 return left + right;
@@ -94,7 +111,7 @@ public @interface LsonNumberOperations
                 return left * right;
             else if(operator == Operator.DIVISION)
                 return left / right;
-            return 0;
+            return 0d;
         }
     }
 }
