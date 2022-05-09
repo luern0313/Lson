c85e0ac92c1e424f09bce20cb872804b4015d227
luern0313 <liupeiran0313@163.com>
2022-05-09 11:59:28 +0800

    调整LsonPrimitive api

--- src/main/java/cn/luern0313/lson/element/LsonPrimitive.java
+++ src/main/java/cn/luern0313/lson/element/LsonPrimitive.java
@@ -6,126 +6,136 @@ package cn.luern0313.lson.element;
  * @author luern0313
  */
 
-public class LsonPrimitive extends LsonElement
-{
+public class LsonPrimitive extends LsonElement {
     private final Object value;
 
-    public LsonPrimitive(Object value)
-    {
+    public LsonPrimitive(Object value) {
         this.value = value;
     }
 
-    public boolean isNull(int index)
-    {
+    public boolean isNull(int index) {
         return get() == null;
     }
 
-    public Object get()
-    {
+    public Object get() {
         return value;
     }
 
-    public boolean isBoolean()
-    {
+    public boolean isBoolean() {
         return value instanceof Boolean;
     }
 
-    public boolean getAsBoolean()
-    {
-        if(isBoolean())
+    public boolean isString() {
+        return value instanceof String;
+    }
+
+    public boolean isNumber() {
+        return value instanceof Number;
+    }
+
+    public boolean isInt() {
+        return value instanceof Integer;
+    }
+
+    public boolean isByte() {
+        return value instanceof Byte;
+    }
+
+    public boolean isShort() {
+        return value instanceof Short;
+    }
+
+    public boolean isFloat() {
+        return value instanceof Float;
+    }
+
+    public boolean isDouble() {
+        return value instanceof Double;
+    }
+
+    public boolean isLong() {
+        return value instanceof Long;
+    }
+
+    public boolean getAsBoolean() {
+        if (isBoolean())
             return (Boolean) value;
         return Boolean.parseBoolean(getAsString());
     }
 
-    public boolean isString()
-    {
-        return value instanceof String;
-    }
-
-    public String getAsString()
-    {
-        if(isBoolean())
+    public String getAsString() {
+        if (isBoolean())
             return String.valueOf(getAsBoolean());
-        else if(isNumber())
+        else if (isNumber())
             return String.valueOf(getAsNumber().toString());
         return (String) value;
     }
 
-    public boolean isNumber()
-    {
-        return value instanceof Number;
-    }
-
-    public Number getAsNumber()
-    {
-        if(isString())
+    public Number getAsNumber() {
+        if (isString())
             return Double.parseDouble((String) value);
         return (Number) value;
     }
 
-    public int getAsInt()
-    {
-        if(isNumber())
+    public int getAsInt() {
+        if (isNumber())
             return getAsNumber().intValue();
         return Integer.parseInt(getAsString());
     }
 
-    public short getAsShort()
-    {
-        if(isNumber())
+    public byte getAsByte() {
+        if (isNumber())
+            return getAsNumber().byteValue();
+        return Byte.parseByte(getAsString());
+    }
+
+    public short getAsShort() {
+        if (isNumber())
             return getAsNumber().shortValue();
         return Short.parseShort(getAsString());
     }
 
-    public float getAsFloat()
-    {
-        if(isNumber())
+    public float getAsFloat() {
+        if (isNumber())
             return getAsNumber().floatValue();
         return Float.parseFloat(getAsString());
     }
 
-    public double getAsDouble()
-    {
-        if(isNumber())
+    public double getAsDouble() {
+        if (isNumber())
             return getAsNumber().doubleValue();
         return Double.parseDouble(getAsString());
     }
 
-    public long getAsLong()
-    {
-        if(isNumber())
+    public long getAsLong() {
+        if (isNumber())
             return getAsNumber().longValue();
         return Long.parseLong(getAsString());
     }
 
-    public Class<?> getValueClass()
-    {
+    public Class<?> getValueClass() {
         return value.getClass();
     }
 
     @Override
-    public String toString()
-    {
-        if(isString())
+    public String toString() {
+        if (isString())
             return "\"" + getAsString() + "\"";
         return getAsString();
     }
 
     @Override
-    public boolean isLsonPrimitive()
-    {
+    public boolean isLsonPrimitive() {
         return true;
     }
 
     @Override
-    public LsonPrimitive getAsLsonPrimitive()
-    {
+    public LsonPrimitive getAsLsonPrimitive() {
         return this;
     }
 
     @Override
-    public LsonElement deepCopy()
-    {
+    public LsonElement deepCopy() {
         return this;
     }
 }
