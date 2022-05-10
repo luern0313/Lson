b23c5c1c7e032d69508442927727e04aec1752c8
luern0313 <liupeiran0313@163.com>
2022-05-10 19:40:40 +0800

    添加java TypeAdapter单测

--- src/test/java/cn/luern0313/lson/DeserializationTest.java
+++ src/test/java/cn/luern0313/lson/DeserializationTest.java
@@ -1,5 +1,6 @@
 package cn.luern0313.lson;
 
+import cn.luern0313.lson.adapter.TypeAdapter;
 import cn.luern0313.lson.constructor.CustomConstructor;
 import cn.luern0313.lson.deserialization.BaseModel;
 import cn.luern0313.lson.deserialization.BaseModel2;
@@ -7,9 +8,14 @@ import cn.luern0313.lson.deserialization.Json1;
 import cn.luern0313.lson.deserialization.Json2;
 import cn.luern0313.lson.deserialization.Json3;
 import cn.luern0313.lson.deserialization.Json4;
+import cn.luern0313.lson.deserialization.Json5;
+import cn.luern0313.lson.element.LsonElement;
+import cn.luern0313.lson.element.LsonPrimitive;
 
 import org.jetbrains.annotations.Nullable;
 import org.junit.Test;
+
+import java.awt.Color;
 import java.lang.reflect.Type;
 
 /**
@@ -34,5 +40,38 @@ public class DeserializationTest {
             }
         }).build();
         Json4.INSTANCE.check(lson4.fromJson(lson4.parse(Json4.INSTANCE.json()), Json4.FeedItemModel.class));
+
+        Lson lson5 = new Lson.LsonBuilder().setTypeAdapter(new TypeAdapter<Color>() {
+            @Override
+            public Color deserialization(LsonElement value) {
+                if (value.isLsonPrimitive())
+                    return Color.decode(((LsonPrimitive) value).getAsString());
+                else if (value.isLsonObject())
+                    return des(value.getAsLsonObject().get("r"), value.getAsLsonObject().get("g"), value.getAsLsonObject().get("b"));
+                else if (value.isLsonArray())
+                    return des(value.getAsLsonArray().get(0), value.getAsLsonArray().get(1), value.getAsLsonArray().get(2));
+                return null;
+            }
+
+            private Color des(LsonElement r, LsonElement g, LsonElement b) {
+                if (r.isLsonPrimitive() && g.isLsonPrimitive() && b.isLsonPrimitive()) {
+                    LsonPrimitive rp = r.getAsLsonPrimitive();
+                    LsonPrimitive gp = g.getAsLsonPrimitive();
+                    LsonPrimitive bp = b.getAsLsonPrimitive();
+
+                    if (rp.isFloat() || rp.isDouble())
+                        return new Color(rp.getAsFloat(), gp.getAsFloat(), bp.getAsFloat());
+                    else
+                        return new Color(rp.getAsInt(), gp.getAsInt(), bp.getAsInt());
+                }
+                return null;
+            }
+
+            @Override
+            public LsonElement serialization(Color obj) {
+                return null;
+            }
+        }).build();
+        Json5.INSTANCE.check(lson5.fromJson(lson5.parse(Json5.INSTANCE.json()), new TypeReference<Json5.ColorModel>(){}));
     }
 }
